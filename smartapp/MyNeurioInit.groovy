ee0736aa6fc661f4a969d8af146abf30d49e0dde
Yves Racine <yracine66@gmail.com>
2015-03-29 17:12:56 -0400

    Create MyNeurioInit.groovy

---
+++ smartapp/MyNeurioInit.groovy
@@ -0,0 +1,81 @@
+/**
+ *  MyNeurioInit
+ *
+ *  Copyright 2014 Yves Racine
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "MyNeurioInit",
+    namespace: "yracine",
+    author: "Yves Racine",
+    description: "My Neurio device Initialization",
+    category: "My Apps",
+    iconUrl: "https://s3-us-west-2.amazonaws.com/neurio/community/NeurioAppLogo60x60.png",
+    iconX2Url: "https://s3-us-west-2.amazonaws.com/neurio/community/NeurioAppLogo72x72.png",
+    iconX3Url: "https://s3-us-west-2.amazonaws.com/neurio/community/NeurioAppLogo72x72.png"
+)
+
+preferences {
+	section("About") {
+		paragraph "MyNeurioInit, the smartapp that initializes your Neurio device and polls it on a regular basis"
+		paragraph "Version 1.0\n\n" +
+			"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
+			"Copyright©2015 Yves Racine"
+		href url: "http://github.com/yracine", style: "embedded", required: false, title: "More information...",
+		description: "http://github.com/yracine/device-type.myneurio.groovy/blob/master/README.md"
+	}
+
+	section("Initialize and regularly polls  this Neurio device") {
+		input "neurio", "capability.powerMeter", title: "Which Neurio"
+	}
+}
+
+def installed() {
+
+	log.debug "installed> calling initialize... "
+	initialize()
+}
+
+
+def updated() {	
+	log.debug "updated> calling initialize... "
+    
+	initialize()
+}
+
+
+def initialize() {
+	neurio.setAuthTokens()
+	neurio.getCurrentUserInfo()
+
+	takeAction()
+    	// set up internal poll timer
+	def pollTimer = 20
+
+	neurio.generateSampleStats("")
+	log.trace "setting poll to ${pollTimer}"
+	schedule("0 0/${pollTimer.toInteger()} * * * ?",takeAction)
+
+}
+
+def takeAction() {
+	log.trace "takeAction>begin"
+	neurio.poll()
+	String nowInLocalTime = new Date().format("HH", location.timeZone)
+    
+	// generate the stats only at the beginning of the day
+	if (nowInLocalTime == "00") {
+		neurio.generateSampleStats("")
+	}
+	log.trace "takeAction>end"
+}
+
