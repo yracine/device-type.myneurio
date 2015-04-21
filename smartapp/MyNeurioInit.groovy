cb72a58fd50ed5d8d78c8eeeb910f3d0e3239d63
Yves Racine <yracine66@gmail.com>
2015-04-20 22:26:04 -0400

    Changed label

--- smartapp/MyNeurioInit.groovy
+++ smartapp/MyNeurioInit.groovy
@@ -34,10 +34,10 @@ preferences {
 			"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 			"Copyright©2015 Yves Racine"
 		href url: "http://github.com/yracine", style: "embedded", required: false, title: "More information...",
-		description: "http://github.com/yracine/device-type.myneurio.groovy/blob/master/README.md"
+		description: "http://github.com/yracine/device-type.myneurio/blob/master/README.md"
 	}
 
-	section("Initialize and regularly polls  this Neurio device") {
+	section("Initializes and regularly polls  this Neurio device") {
 		input "neurio", "capability.powerMeter", title: "Which Neurio"
 	}
 }
@@ -72,7 +72,8 @@ def initialize() {
 def takeAction() {
 	log.trace "takeAction>begin"
 	neurio.poll()	
-/*	
+
+/*
 	get_neurio_appliances_data()
 */    
 	log.trace "takeAction>end"
@@ -83,6 +84,11 @@ private def get_neurio_appliances_data() {
 	Boolean foundAppliance=false		
 	def applianceList
 	def applianceFields
+	String applianceName
+	def applianceLabel
+	def applianceTags
+	def applianceCreated
+	def applianceUpdated
 
 	try {
 		neurio.getApplianceList("")
@@ -116,24 +122,40 @@ private def get_neurio_appliances_data() {
 			log.debug "get_neurio_appliances_data>applianceFields = $applianceFields"
 
 			if (applianceFields?.size() > 0) {
-				def applianceName=applianceFields?.name
-				def applianceLabel=applianceFields?.label
-				def applianceTags=applianceFields?.tags
-				def applianceCreated=applianceFields?.createdAt
-				def applianceUpdated=applianceFields?.updatedAt
+				applianceName=applianceFields?.name.toString()
+				applianceLabel=applianceFields?.label
+				applianceTags=applianceFields?.tags
+				applianceCreated=applianceFields?.createdAt
+				applianceUpdated=applianceFields?.updatedAt
 				log.debug "get_neurio_appliances_data>applianceId= ${applianceId}, applianceName=${applianceName}" +
 					",applianceLabel=${applianceLabel},applianceTags=${applianceTags},created=${applianceCreated}, updated=${applianceUpdated}"
 				            
-    			}
-			// generate Appliance stats & events for yesterday
-		
-			neurio.generateAppliancesStats("",applianceId,startDate,endDate,"days")
-			neurio.generateAppliancesEvents("",applianceId,startDate,endDate)
+			}
+			def dni = [app.id, getNeurioApplianceChildName(),applianceName, applianceId].join('.')
+			def d = getChildDevice(dni)
+
+			if (!d) {
+				def labelName = "${getNeurioApplianceChildName()} ${applianceName}"
+
+				log.debug "About to create child device with id $dni, labelName=  ${labelName}"
+				d = addChildDevice(getChildNamespace(), getNeurioApplianceChildName(), dni, null, [label: "${labelName}", completedSetup: true])
+				d.initialSetup("", applianceId)
+				log.debug "created ${d.displayName} with id $dni"
+			} else {
+				log.debug "initialize>found ${d.displayName} with id $dni already exists"
+			}            
+
+			// generate Appliance stats & events since yesterday
+			d.poll()					
 		} /* end for */            
         
-		// generate Location stats & events for yesterday
-		neurio.generateAppliancesStats("","",startDate,endDate,"days")
-		neurio.generateAppliancesEvents("","",startDate,endDate)
-
 	}    
 }
+
+def getChildNamespace() {
+	"yracine"
+}
+def getNeurioApplianceChildName() {
+	"My Neurio Appliance"
+}
+
