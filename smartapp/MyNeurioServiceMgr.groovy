a25ac7cbf4e724c8f376d91e895ccb144e20933e
Yves Racine <yracine66@gmail.com>
2015-10-24 08:01:13 -0400

    v0.9.4 changed rescheduleIfNeeded() signature

--- smartapp/MyNeurioServiceMgr.groovy
+++ smartapp/MyNeurioServiceMgr.groovy
@@ -63,7 +63,7 @@ def about() {
  		section("About") {	
 			paragraph "MyNeurioServiceMgr, the smartapp that connects your Neurio Sensor(s) to SmartThings via cloud-to-cloud integration" +
 				" and polls your Neurio appliance data on a regular interval"
-			paragraph "Version 0.9.3\n" 
+			paragraph "Version 0.9.4\n" 
 			paragraph "If you like this smartapp, please support the developer via PayPal and click on the Paypal link below " 
 				href url: "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=yracine%40yahoo%2ecom&lc=US&item_name=Maisons%20ecomatiq&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest",
 					title:"Paypal donation..."
@@ -530,7 +530,8 @@ def initialize() {
 }
 
 
-def rescheduleIfNeeded() {
+def rescheduleIfNeeded(evt) {
+	log.debug("rescheduleIfNeeded>$evt.name=$evt.value")
 	Integer delay = givenInterval ?: 20 // By default, do poll every 20 min.
 	BigDecimal currentTime = now()    
 	BigDecimal lastPollTime = (currentTime - (state?.poll["last"]?:0))  
